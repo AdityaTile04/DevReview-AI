a83f2bad176c6b92c9ab2c5fbd230bf6c2a883ce
Aditya Tile <adityatile99@gmail.com>
2025-12-26 13:48:58 +0530

    feat: add review history and review detail APIs

--- backend/src/controllers/review.controller.ts
+++ backend/src/controllers/review.controller.ts
@@ -55,3 +55,49 @@ export const createReview = async (req: AuthRequest, res: Response) => {
     });
   }
 };
+
+export const getReviews = async (req: AuthRequest, res: Response) => {
+  const userId = req.user!.userId;
+
+  try {
+    const result = await pool.query(
+      `
+              SELECT id, language, framework, score, created_at
+              FROM reviews
+              WHERE user_id = $1
+              ORDER BY created_at DESC
+              `,
+      [userId]
+    );
+
+    res.json({
+      reviews: result.rows,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch reviews" });
+  }
+};
+
+export const getReviewsById = async (req: AuthRequest, res: Response) => {
+  const userId = req.user!.userId;
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+      SELECT *
+      FROM reviews
+      WHERE id = $1 AND user_id = $2
+      `,
+      [id, userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+
+    res.json({ review: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch review" });
+  }
+};
--- backend/src/routes/review.route.ts
+++ backend/src/routes/review.route.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
-import { authMiddleware } from '../middlewares/auth.midddleware';
-import { createReview } from '../controllers/review.controller';
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.midddleware";
+import {
+  createReview,
+  getReviews,
+  getReviewsById,
+} from "../controllers/review.controller";
 
 const router = Router();
 
-router.post('/', authMiddleware, createReview)
+router.post("/", authMiddleware, createReview);
+router.get("/", authMiddleware, getReviews);
+router.get("/:id", authMiddleware, getReviewsById);
 
 export default router;
